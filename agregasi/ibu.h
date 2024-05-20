b71896050ac735a64123c84d4c080ee0d472fc90
Qilqilll <aqilf612@gmail.com>
2024-05-20 11:58:43 +0700

    jadikan public

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -3,3 +3,26 @@
 #include <vector>
 
 class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu\"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu\"" << nama << "\" tidak ada\n";
+
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
+
+void ibu::cetakAnak() {
+	cout << "Daftar anak dari ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif

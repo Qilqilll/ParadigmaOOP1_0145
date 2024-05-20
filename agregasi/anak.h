bd57b6ff0481f143dadb32950849cfb7acddb189
Qilqilll <aqilf612@gmail.com>
2024-05-20 11:54:58 +0700

    header daari agregasi

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -1,15 +1,3 @@
 #ifndef anak_h
 #define anak_h
 
-class anak {
-public:
-	string nama;
-	anak(string pNama) :nama(pNama) {
-		cout << "Anak \"" << nama << "\" ada\n";
-	}
-	~anak() {
-		cout << "Anak \"" << nama << "\" tidak ada\n";
-
-	}
-};
-#endif

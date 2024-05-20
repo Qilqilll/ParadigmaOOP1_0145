6b6101367ae7e50a9372f78bac6292dbdf58f6fc
Qilqilll <aqilf612@gmail.com>
2024-05-20 11:56:27 +0700

    jadikan public

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -2,3 +2,13 @@
 #define anak_h
 
 class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+
+	}
+};
